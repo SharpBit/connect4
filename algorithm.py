d086cbcb3b8b18e72cd6c6ec25b31f34f5e5717f
SharpBit <31069084+SharpBit@users.noreply.github.com>
2022-12-02 02:32:03 -0500

    refactor algorithm.py so all the diff dirs use a singel function check_dir

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 def get_best_move(bd):
     '''
     For the 7 open spots, this function calculates which one has the greatest score
@@ -12,169 +15,98 @@ def get_best_move(bd):
 
     max_score = 0
     best_move = 3  # default to the center column
-    for col in range(7):
+    for col in range(bd.COLS):
         move = (bd.first_empty[col], col)
 
         if bd.is_col_full(move[0]):  # Col is full, skip
             continue
         print('COLUMN:', col)
-        target = None
         scores = [0, 0]  # [AI score, opponent score]
 
+        dists_to_edge = {}
         # move[0] is the number of rows below the current row
         # For example, there are 0 rows below row 0
         # Since the end bound of range() is exclusive, we add 1
-        rows_below = move[0] + 1
+        dists_to_edge[(-1, 0)] = move[0] + 1
         # bd.ROWS - move[0] - 1 is the number of rows above the current row
         # For example, there are 0 rows above row 5 (6 - 5 - 1 = 0)
         # Since the end bound of range() is exclusive, we add 1, cancelling out the -1
-        rows_above = bd.ROWS - move[0]
+        dists_to_edge[(1, 0)] = bd.ROWS - move[0]
         # move[1] is the number of rows to the left of the current col
         # For example, there are 0 rows to the right of col 0
         # Since the end bound of range() is exclusive, we add 1
-        cols_left = move[1] + 1
+        dists_to_edge[(0, -1)] = move[1] + 1
         # bd.COLS - move[1] - 1 is the number of rows to the right of the current col
         # For example, there are 0 rows to the right of col 6 (7 - 6 - 1 = 0)
         # Since the end bound of range() is exclusive, we add 1, cancelling out the -1
-        cols_right = bd.COLS - move[1]
+        dists_to_edge[(0, 1)] = bd.COLS - move[1]
+
+        def check_dir(direction: Tuple[int, int], board) -> bool:
+            target = None
+            dists_to_check = [4]
+            if direction[0] != 0:
+                dists_to_check.append(dists_to_edge[(direction[0], 0)])
+            if direction[1] != 0:
+                dists_to_check.append(dists_to_edge[(0, direction[1])])
+
+            for offset in range(1, min(dists_to_check)):
+                # don't need to declare move as nonlocal since we are not reassigning
+                piece = board.get_position(move[0] + offset * direction[0], move[1] + offset * direction[1])
+                if target is None and piece == board.EMPTY:
+                    continue
+                if target is None:
+                    target = piece
+                if piece == target:
+                    # don't need to declare scores as nonlocal since we are modifying, not reassigning
+                    scores[target - 1] += 1
+                    if scores[0] >= 3 or scores[1] >= 3:
+                        # either we win or we block a winning spot
+                        return True
+                elif piece != board.EMPTY:  # opponent piece
+                    break
+
+            # It's fine to update scores for each direction and not each axis
+            # since if the score increases in the other direction on the same axis later,
+            # the score will update and reflect that
+            nonlocal max_score
+            nonlocal best_move
+            if scores[0] > max_score:
+                max_score = scores[0]
+                best_move = move[1]
+            return False
 
         # going down
-        for dr in range(1, min(rows_below, 4)):
-            piece = bd.get_position(move[0] - dr, move[1])
-            if not target and piece == bd.EMPTY:
-                continue
-            if not target:
-                target = piece
-            if piece == target:
-                scores[target - 1] += 1
-                if scores[0] >= 3 or scores[1] >= 3:
-                    # either we win or we block a winning spot
-                    return move[1]
-            elif piece != bd.EMPTY:  # opponent piece
-                break
-
+        if check_dir((-1, 0), bd):
+            return move[1]
         print(f'down {scores=}')
-        # resetting scores
-        if scores[0] > max_score:
-            max_score = scores[0]
-            best_move = move[1]
-        target = None
         scores = [0, 0]
 
         # going left
-        for dc in range(1, min(cols_left, 4)):
-            piece = bd.get_position(move[0], move[1] - dc)
-            if not target and piece == bd.EMPTY:
-                continue
-            if not target:
-                target = piece
-            if piece == target:
-                scores[target - 1] += 1
-                if scores[0] >= 3 or scores[1] >= 3:
-                    # either we win or we block a winning spot
-                    return move[1]
-            elif piece != bd.EMPTY:  # opponent piece
-                break
-
-        target = None
+        if check_dir((0, -1), bd):
+            return move[1]
         # going right
-        for dc in range(1, min(cols_right, 4)):
-            piece = bd.get_position(move[0], move[1] + dc)
-            if not target and piece == bd.EMPTY:
-                continue
-            if not target:
-                target = piece
-            if piece == target:
-                scores[target - 1] += 1
-                if scores[0] >= 3 or scores[1] >= 3:
-                    # either we win or we block a winning spot
-                    return move[1]
-            elif piece != bd.EMPTY:  # opponent piece
-                break
-
+        if check_dir((0, 1), bd):
+            return move[1]
         print(f'left/right {scores=}')
-        # resetting scores
-        if scores[0] > max_score:
-            max_score = scores[0]
-            best_move = move[1]
-        target = None
         scores = [0, 0]
 
-        # going down right
-        for diag_offset in range(1, min(rows_below, cols_right, 4)):
-            piece = bd.get_position(move[0] - diag_offset, move[1] + diag_offset)
-            if not target and piece == bd.EMPTY:
-                continue
-            if not target:
-                target = piece
-            if piece == target:
-                scores[target - 1] += 1
-                if scores[0] >= 3 or scores[1] >= 3:
-                    # either we win or we block a winning spot
-                    return move[1]
-            elif piece != bd.EMPTY:  # opponent piece
-                break
-
-        target = None
+        # down right
+        if check_dir((-1, 1), bd):
+            return move[1]
         # up left
-        for diag_offset in range(1, min(rows_above, cols_left, 4)):
-            piece = bd.get_position(move[0] + diag_offset, move[1] - diag_offset)
-            if not target and piece == bd.EMPTY:
-                continue
-            if not target:
-                target = piece
-            if piece == target:
-                scores[target - 1] += 1
-                if scores[0] >= 3 or scores[1] >= 3:
-                    # either we win or we block a winning spot
-                    return move[1]
-            elif piece != bd.EMPTY:  # opponent piece
-                break
-
+        if check_dir((1, -1), bd):
+            return move[1]
         print(f'down right/up left {scores=}')
-        # resetting scores
-        if scores[0] > max_score:
-            max_score = scores[0]
-            best_move = move[1]
-        target = None
         scores = [0, 0]
 
         # down left
-        for diag_offset in range(1, min(rows_below, cols_left, 4)):
-            piece = bd.get_position(move[0] - diag_offset, move[1] - diag_offset)
-            if not target and piece == bd.EMPTY:
-                continue
-            if not target:
-                target = piece
-            if piece == target:
-                scores[target - 1] += 1
-                if scores[0] >= 3 or scores[1] >= 3:
-                    # either we win or we block a winning spot
-                    return move[1]
-            elif piece != bd.EMPTY:  # opponent piece
-                break
-
-        target = None
+        if check_dir((-1, -1), bd):
+            return move[1]
         # up right
-        for diag_offset in range(1, min(rows_above, cols_right, 4)):
-            piece = bd.get_position(move[0] + diag_offset, move[1] + diag_offset)
-            if not target and piece == bd.EMPTY:
-                continue
-            if not target:
-                target = piece
-            if piece == target:
-                scores[target - 1] += 1
-                if scores[0] >= 3 or scores[1] >= 3:
-                    # either we win or we block a winning spot
-                    return move[1]
-            elif piece != bd.EMPTY:  # opponent piece
-                break
-
+        if check_dir((1, 1), bd):
+            return move[1]
         print(f'down left/up right {scores=}')
-        # resetting scores
-        if scores[0] > max_score:
-            max_score = scores[0]
-            best_move = move[1]
+
         print(f'{col=} {max_score=} {best_move=}\n')
 
     return best_move
